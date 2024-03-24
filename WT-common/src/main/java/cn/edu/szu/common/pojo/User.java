0a65cfbbcd6ed5648b26da6c7a141e9c0468a35b
YWF <1497838743@qq.com>
2024-03-24 20:59:24 +0800

    test

---
+++ WT-common/src/main/java/cn/edu/szu/common/pojo/User.java
@@ -0,0 +1,10 @@
+package cn.edu.szu.common.pojo;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Long id;
+    private String userName;
+    private String passWord;
+}
---
+++ WT-common/src/main/java/cn/edu/szu/common/utils/JWTUtil.java
@@ -0,0 +1,33 @@
+package cn.edu.szu.common.utils;
+
+
+import cn.edu.szu.common.pojo.User;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JWTUtil {
+    public static String getToken(User user){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id",user.getId());
+        claims.put("username",user.getUserName());
+        String token = JWT.create()
+                .withClaim("user",claims)
+                .withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 60 ))
+                .sign(Algorithm.HMAC256("ywf"));
+        System.out.println(token);
+        return token;
+    }
+    public static void validToken(String token){
+        JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256("ywf")).build();
+        DecodedJWT decodedJWT = jwtVerifier.verify(token);
+        Map<String, Claim> claims = decodedJWT.getClaims();
+        System.out.println(claims.get("user"));
+    }
+}
