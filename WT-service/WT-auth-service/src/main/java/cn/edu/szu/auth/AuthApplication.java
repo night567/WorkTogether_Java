8321b0b51907aee4954f634230cc965d90ed53cc
wqy <2839468956@qq.com>
2024-03-26 22:14:37 +0800

    分页查询所有资源

--- WT-service/WT-auth-service/src/main/java/cn/edu/szu/auth/AuthApplication.java
+++ WT-service/WT-auth-service/src/main/java/cn/edu/szu/auth/AuthApplication.java
@@ -1,12 +1,14 @@
 package cn.edu.szu.auth;
 
 import cn.edu.szu.feign.config.DefaultFeignConfig;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "cn.edu.szu.feign.client", defaultConfiguration = DefaultFeignConfig.class)
+@MapperScan("cn.edu.szu.auth.mapper")
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
