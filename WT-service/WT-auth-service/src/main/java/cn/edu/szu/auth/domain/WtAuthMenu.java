6f87c2ec9daf76ebcdcfab98ed0dd7aed16dcf13
YWF <1497838743@qq.com>
2024-03-27 20:18:35 +0800

    complete role and role-auth

---
+++ WT-service/WT-auth-service/src/main/java/cn/edu/szu/auth/domain/WtAuthMenu.java
@@ -0,0 +1,80 @@
+package cn.edu.szu.auth.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import lombok.Data;
+
+/**
+ * 菜单
+ * @TableName wt_auth_menu
+ */
+@TableName(value ="wt_auth_menu")
+@Data
+public class WtAuthMenu implements Serializable {
+    /**
+     * 主键
+     */
+    @TableId(value = "id")
+    private Long id;
+
+    /**
+     * 菜单名称
+     */
+    @TableField(value = "name")
+    private String name;
+
+    /**
+     * 功能描述
+     */
+    @TableField(value = "describe")
+    private String describe;
+
+    /**
+     * 是否公开菜单
+就是无需分配就可以访问的。所有人可见
+     */
+    @TableField(value = "is_public")
+    private Boolean isPublic;
+
+    /**
+     * 对应路由path
+     */
+    @TableField(value = "path")
+    private String path;
+
+    /**
+     * 对应路由组件component
+     */
+    @TableField(value = "component")
+    private String component;
+
+    /**
+     * 状态(是否可用 0:不可用 1:可用)
+     */
+    @TableField(value = "is_enable")
+    private Boolean isEnable;
+
+    /**
+     * 排序
+     */
+    @TableField(value = "sort_value")
+    private Integer sortValue;
+
+    /**
+     * 菜单分组
+     */
+    @TableField(value = "group")
+    private String group;
+
+    /**
+     * 父级菜单id
+     */
+    @TableField(value = "parent_id")
+    private Long parentId;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
---
+++ WT-service/WT-auth-service/src/main/java/cn/edu/szu/auth/mapper/WtAuthMenuMapper.java
@@ -0,0 +1,18 @@
+package cn.edu.szu.auth.mapper;
+
+import cn.edu.szu.auth.domain.WtAuthMenu;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 86199
+* @description 针对表【wt_auth_menu(菜单)】的数据库操作Mapper
+* @createDate 2024-03-27 20:18:11
+* @Entity cn.edu.szu.auth.domain.WtAuthMenu
+*/
+public interface WtAuthMenuMapper extends BaseMapper<WtAuthMenu> {
+
+}
+
+
+
+
