89103869b8422a42afefb764829331d826924dd4
wqy <2839468956@qq.com>
2024-04-03 21:12:33 +0800

    成员列表

--- WT-service/WT-user-service/src/main/java/cn/edu/szu/user/UserApplication.java
+++ WT-service/WT-user-service/src/main/java/cn/edu/szu/user/UserApplication.java
@@ -8,9 +8,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "cn.edu.szu.feign.client", defaultConfiguration = DefaultFeignConfig.class)
-@MapperScan("cn.edu.szu.user.mapper")
-@ComponentScan("cn.edu.szu.user.dao")
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
