b63498431921d4f4f4c8ac81ce145a6b6273410b
YWF <1497838743@qq.com>
2024-04-14 14:40:31 +0800

    修改查询所有角色为   ==>   查询根据公司id查询公司的所有角色

--- WT-service/WT-auth-service/src/main/java/cn/edu/szu/auth/service/impl/AuthRoleServiceImpl.java
+++ WT-service/WT-auth-service/src/main/java/cn/edu/szu/auth/service/impl/AuthRoleServiceImpl.java
@@ -13,7 +13,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
 * @author 86199
